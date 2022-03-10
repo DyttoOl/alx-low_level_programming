50c878b2eb63946640c625bd994adb289160529a
DyttoOl <edithlangot@gmail.com>
2022-03-10 02:02:50 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+1 #include <stdio.h>
+2 /**
+3 * main - entry point
+4 *
+5 * Return: 0 number
+6 */
+7 int main(void)
+8 {
+9         puts("\"Programming is like building a multilingual puzzle");
+10        return (0);
+11 }
