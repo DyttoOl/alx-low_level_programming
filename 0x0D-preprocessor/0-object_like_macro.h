ebb18e23727eeea74383f10e134573aebd301ddb
DyttoOl <65666591+DyttoOl@users.noreply.github.com>
2022-04-11 12:52:11 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 #ifdef _0_OBJECT_LIKE_MACRO_H_
 #define _0_OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
 #endif /* _0_OBJECT_LIKE_MACRO_H_ */
-=======
-#ifndef _0_OBJECT_LIKE_MACRO_H_
-#define _0_OBJECT_LIKE_MACRO_H_
-
-#define SIZE 1024
-
-#endif /* _0_OBJECT_LIKE_MACRO_H_ */
->>>>>>> bde13a7f9fca2f16c0b8596b8171fe31b8855f05
