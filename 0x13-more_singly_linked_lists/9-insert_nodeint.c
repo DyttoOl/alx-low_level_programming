e816c2b46eeb02a2dc5eb6e9b8652324439023af
DyttoOl <65666591+DyttoOl@users.noreply.github.com>
2022-04-26 13:00:31 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,52 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * insert_nodeint_at_index -  insert a node in a idx position
- * @head: pointer to head in function
- * @idx: index or position to be added
- * @n: node's value
- * Return: nth node data
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+ * insert_nodeint_at_index - inserts node at given position
+ * @head: pointer to head of list
+ * @index: index of new node
+ * @n: value for data element
+ * Return: address of new node, NULL if fails
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-	unsigned int nodes;
-	listint_t *node_index = *head;
-	listint_t *new_node, *node_after;
+	listint_t *prior, *new;
+	unsigned int i;
 
-	if (head == NULL || *head == NULL)/*Ask if there is not input*/
+	if (head == NULL)
 		return (NULL);
-
-	for (nodes = 0; node_index != NULL; nodes++)/*Count the num of nodes*/
-		node_index = node_index->next;
-	if (idx > (nodes + 1))/*Allows insert until after-last node*/
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-
-	node_index = *head;
-
-	new_node = malloc(sizeof(listint_t));/*allocate and fill the new node*/
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-
-	if (idx == 0)
+	new->n = n;
+	new->next = NULL;
+	if (index == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (*head);
 	}
-	else
+	new->next = (*head)->next;
+	prior = *head;
+	for (i = 1; i < index; i++)
 	{
-		node_after = *head;
-		for (nodes = 0; nodes < (idx - 1); nodes++)
-			node_index = node_index->next;
-		for (nodes = 0; nodes < idx; nodes++)
-			node_after = node_after->next;
-		node_index->next = new_node;
-		new_node->next = node_after;
-		return (new_node);
 
+		prior = prior->next;
+		if (prior == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->next = prior->next;
+
+		if (new->next == NULL && i != index - 1)
+		{
+			free(new);
+			return (NULL);
+		}
 	}
+	new->next = prior->next;
+	prior->next = new;
+	return (new);
 }
